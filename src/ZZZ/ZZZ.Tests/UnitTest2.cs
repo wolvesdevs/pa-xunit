30f4d7236d6295b993ffa80bd1ef4c29c6e65a18
wolvesdevs_workPC <75551104+wolvesdevs@users.noreply.github.com>
2024-02-26 13:20:27 +0900

    refs #026 テストのSkip

--- src/ZZZ/ZZZ.Tests/UnitTest2.cs
+++ src/ZZZ/ZZZ.Tests/UnitTest2.cs
@@ -1,4 +1,4 @@
-﻿namespace ZZZ.Tests;
+﻿namespace ZZZ.Tests { }
 
 public class UnitTest2
 {
@@ -7,37 +7,43 @@ public class UnitTest2
     public void TestA()
     {
     }
-    
+
     [Fact]
     [Trait("Category", "受注")]
     public void TestB()
     {
     }
-    
+
     [Fact]
     [Trait("Category", "在庫")]
     public void TestC()
     {
     }
-    
+
     [Fact]
     [Trait("Category", "受注")]
     [Trait("Category", "在庫")]
     public void TestD()
     {
     }
-    
+
     [Fact]
     [Trait("Category", "受注")]
     [Trait("Priority", "High")]
     public void TestE()
     {
     }
-    
+
     [Fact]
     [Trait("Owner", "XX開発Gr")]
     public void TestZ()
     {
+    }
+
+    [Fact(Skip = "バージョンアップに伴いこのテストは不要")]
+    public void TestXXX()
+    {
+        Assert.Equal(1, 2);
     }
 
 }
