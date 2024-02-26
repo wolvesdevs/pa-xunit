b0da6c0b4dd9cb43f4b21c37d179212fa2dc84d2
wolvesdevs_workPC <75551104+wolvesdevs@users.noreply.github.com>
2024-02-26 13:28:05 +0900

    refs #028 ログの出力

---
+++ src/ZZZ/ZZZ.Tests/UnitTest3.cs
@@ -0,0 +1,18 @@
+﻿using System.Diagnostics;
+using Xunit.Abstractions;
+
+namespace ZZZ.Tests;
+
+public class UnitTest3(ITestOutputHelper outputhelper)
+{
+    private readonly ITestOutputHelper _outputhelper = outputhelper;
+
+    [Fact]
+    public void ログの出力()
+    {
+        Debug.WriteLine("ログの出力");
+        _outputhelper.WriteLine("ログの出力: IN");
+        Assert.Equal(1, 1);
+        _outputhelper.WriteLine("ログの出力: OUT");
+    }
+}
