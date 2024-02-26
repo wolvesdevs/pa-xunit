91d07eff2d6aee8c8d47fa6b877845f26791fc31
wolvesdevs_workPC <75551104+wolvesdevs@users.noreply.github.com>
2024-02-26 11:43:56 +0900

    refs #025 テストの分類方法_Trait

---
+++ src/ZZZ/ZZZ.Tests/UnitTest2.cs
@@ -0,0 +1,43 @@
+﻿namespace ZZZ.Tests;
+
+public class UnitTest2
+{
+    [Fact]
+    [Trait("Category", "受注")]
+    public void TestA()
+    {
+    }
+    
+    [Fact]
+    [Trait("Category", "受注")]
+    public void TestB()
+    {
+    }
+    
+    [Fact]
+    [Trait("Category", "在庫")]
+    public void TestC()
+    {
+    }
+    
+    [Fact]
+    [Trait("Category", "受注")]
+    [Trait("Category", "在庫")]
+    public void TestD()
+    {
+    }
+    
+    [Fact]
+    [Trait("Category", "受注")]
+    [Trait("Priority", "High")]
+    public void TestE()
+    {
+    }
+    
+    [Fact]
+    [Trait("Owner", "XX開発Gr")]
+    public void TestZ()
+    {
+    }
+
+}
