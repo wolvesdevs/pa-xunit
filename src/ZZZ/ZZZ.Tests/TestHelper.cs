4ad3a19501d35acecf5392eaadcab1388ce14e31
wolvesdevs_workPC <75551104+wolvesdevs@users.noreply.github.com>
2024-02-26 15:39:41 +0900

    refs #036 InlineDataをCSVファイルから読み込む方法

---
+++ src/ZZZ/ZZZ.Tests/TestHelper.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel;
+
+namespace ZZZ.Tests;
+
+public class TestHelper
+{
+    public static IEnumerable<object[]> CsvRead()
+    {
+        string filePath = "XXX.csv";
+        if (!File.Exists(filePath))
+        {
+            throw new FileNotFoundException("XXXX" + filePath);
+        }
+
+        var lines = File.ReadAllLines(filePath);
+        var result = new List<object[]>();
+        foreach (var line in lines)
+        {
+            var items = line.Split(',');
+            result.Add(items);
+        }
+
+        return result;
+    }
+}
---
+++ src/ZZZ/ZZZ.Tests/UnitTest9.cs
@@ -0,0 +1,16 @@
+﻿using ZZZ.UI.ViewModels;
+
+namespace ZZZ.Tests;
+
+public class UnitTest9
+{
+    [Theory]
+    [MemberData(nameof(TestHelper.CsvRead), MemberType = typeof(TestHelper))]
+    public void TestXD(int price, string result)
+    {
+        ProductViewModel vm = new();
+
+        vm.Price = price;
+        Assert.Equal(result, vm.GetTaxPrice());
+    }
+}
